aeabefe5f7220016eb5cfb55e029d156947d7701
Arbol <tringuy1993@gmail.com>
2024-01-01 19:51:58 -0800

    initial commit for Greek Time

--- components/ECharts/Time/EChartTime.tsx
+++ components/ECharts/Time/EChartTime.tsx
@@ -1,22 +1,26 @@
 import React from "react";
 import { EChartTime_Opts } from "./EChartTime_Opts";
 import { EChartThemed } from "../EChartThemed";
+import useCustomSWR from "@/lib/fetchdata/fetch-custom";
+import { ALL_URL } from "@/lib/fetchdata/apiURLs";
+import { modify_time_data } from "../UtilECharts";
 
-const EChartTime = ({ data }) => {
-  let option, chartHeight, chartOptions;
-  if (data) {
-    option = EChartTime_Opts(data);
-    chartOptions = option.option;
-    chartHeight = option.chartHeight;
+const EChartTime = ({ params }) => {
+  const { data, isLoading } = useCustomSWR(ALL_URL, params);
+
+  if (!data || isLoading) {
+    return <div> Is Loading</div>;
   }
+
+  const modifiedData = modify_time_data(data?.data, params.greek).modified_data;
+  const { option: chartOptions, chartHeight } = EChartTime_Opts(modifiedData);
+
   return (
-    <>
-      <EChartThemed
-        option={{ ...chartOptions }}
-        style={{ height: chartHeight }}
-        notMerge={true}
-      ></EChartThemed>
-    </>
+    <EChartThemed
+      option={chartOptions}
+      style={{ height: chartHeight }}
+      notMerge={true}
+    />
   );
 };
 
