5cb67b0272e32ec606553dba4925d8a5726feb36
Arbol <102921107+tringuy1993@users.noreply.github.com>
2024-01-08 17:13:35 -0800

    Completed Live 0DTE (#10)
    * Initial Commit
    
    * completed Live0DTE

--- components/ECharts/Live0DTE/EChart0DTE.tsx
+++ components/ECharts/Live0DTE/EChart0DTE.tsx
@@ -1,15 +1,32 @@
-import React from "react";
-import { Grid } from "@mantine/core";
-import { combineESOptionData } from "../DataEChart";
+import React, { useEffect, useState } from "react";
 import { EChart0DTE_Opts } from "./EChart0DTE_Opts";
 import { EChartThemed } from "../EChartThemed";
+import useCustomSWR from "@/lib/fetchdata/fetch-custom";
+import { LIVE_OTM_URL } from "@/lib/fetchdata/apiURLs";
+import MainLoading from "@/app/loading";
 
-const EChart0DTE = ({ data }) => {
-  let ecOptions;
-  if (data) {
-    ecOptions = EChart0DTE_Opts(data);
+const EChart0DTE = ({ params }) => {
+  const [refreshInterval, setRefreshInterval] = useState<number>();
+
+  useEffect(() => {
+    //Update Refresh Interval based on the date selected.
+    const today = new Date().toISOString().slice(0, 10);
+    if (params.date === today) {
+      setRefreshInterval(60000);
+    } else {
+      setRefreshInterval(0);
+    }
+  }, [params]);
+
+  const { data, isLoading } = useCustomSWR(LIVE_OTM_URL, params, {
+    refreshInterval: refreshInterval,
+  });
+
+  if (!data || isLoading) {
+    return <MainLoading />;
   }
 
-  return <EChartThemed option={{ ...ecOptions }} style={{ height: "650px" }} />;
+  const ecOptions = EChart0DTE_Opts(data.data);
+  return <EChartThemed option={ecOptions} style={{ height: "650px" }} />;
 };
 export default EChart0DTE;
